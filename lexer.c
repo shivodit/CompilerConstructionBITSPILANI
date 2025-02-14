89d2cceecc709243ae2627b5fd94994f5c5dfd37
Omkar BITS <f20220089@pilani.bits-pilani.ac.in>
2025-02-15 00:18:06 +0530

    First draft of getStream(FILE *fp) function

--- lexer.c
+++ lexer.c
@@ -1,4 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define BUFFER_SIZE 4096 // buffer size of a single buffer
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------*/
+char* twinBuffer[2]; // twin buffer
+bool bufferToBeLoaded = false; // represents the buffer to be loaded next
+// bool completeFileRead = false; // stores whether the input file has been completely read
+
+void initializeTwinBuffer(){
+    twinBuffer[0] = (char*)malloc(BUFFER_SIZE*sizeof(char));
+    twinBuffer[1] = (char*)malloc(BUFFER_SIZE*sizeof(char));
+    
+    bufferToBeLoaded = 0;
+}
+
+FILE *getStream(FILE *fp){
+    if(fp == NULL){ // or if(completeFileRead)
+        return NULL; // or return fp ?
+    }
+
+    memset(twinBuffer[bufferToBeLoaded], EOF, BUFFER_SIZE); 
+    // initializing the buffer with EOF so that if the reamining data in file is lesser than BUFFER_SIZE, the remaining part of the buffer is filled with EOFs
+
+    int charactersRead = read(fp, twinBuffer[bufferToBeLoaded], BUFFER_SIZE);
+    if(charactersRead == 0 || charactersRead < BUFFER_SIZE){
+        // completeFileRead = true;
+        return NULL; // or return fp ?
+    }
+    if(charactersRead == -1){
+        printf("Error loading buffer\n");
+    }
+
+    bufferToBeLoaded = !bufferToBeLoaded;
+    return fp;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
 
 void removeComments(char *testcaseFile, char *cleanFile){
     FILE* TEST = fopen(testcaseFile, "r");
