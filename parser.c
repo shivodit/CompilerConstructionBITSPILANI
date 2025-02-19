370df9412cb579f36f00e01089df2dc002b0c695
anon <f20221221@pilani.bits-pilani.ac.in>
2025-02-19 20:09:07 +0530

    init parser created

--- parser.c
+++ parser.c
@@ -4,6 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+RuleList* grammar;
+FirstAndFollow* F;
+ParseTable* T;
+
+initailizeparser(char* grammarfile, char* first, char* follow){
+    grammar = readRules(grammarfile);
+    F = readFirstAndFollowSets(first, follow);
+    T = createParseTable(*F);
+}
+
 RuleList* readRules(char* grammarfile){
     // TODO
     return NULL;
@@ -23,31 +33,63 @@ ParseTable* createParseTable(FirstAndFollow F){
     return NULL;
 }
 
-TreeNode* parseInputSourceCode(char *testcasefile, ParseTable T, bool verbose){
+TreeNode* parseInputSourceCode(char *testcasefile,  bool verbose){
+
     Stack* stack = createStack();
+    push(stack, (Symbol){true, .symbol.nt = PROGRAM});
+
     TreeNode* parse_tree = createTreeNode((Symbol){false, .symbol.nt=PROGRAM}, false, NULL);
-
-    RuleList* rules = readRules("final_grammar_index_rep.txt");
-    // FirstAndFollow* F = ComputeFirstAndFollowSets(*rules);
-    // ParseTable* T = createParseTable(*F);
-
 
     // init lexer
     FILE* fp = fopen(testcasefile, "r");
     initializelexer(fp);
     int token_count = 0;
 
-    while (!has_file_ended()){
+    TreeNode* parentNode = parse_tree;
+
+    while (true){
         tokenInfo* curr_token = getNextToken();
-        if (curr_token == NULL) continue;
         token_count++;
+        
+        // main logic
+        Symbol* top_symbol = top(stack);
+
+        
+        if (curr_token == NULL) {
+            if (verbose) {
+                printf("End of file reached or Error\n");
+            }
+            
+            // STOP CONDITION
+            if (has_file_ended() && top_symbol->is_terminal && top_symbol->symbol.t == DOLLAR){
+                break;             
+            }
+            else if (has_file_ended()){
+                // ERROR
+                printf("Error: Unexpected end of file\n");
+                break;
+            }
+
+            // TOKEN ERROR {DECIDE WHETHER TO INVOKE PANIC MODE}
+            continue;
+        }
+
         if (verbose) {
             printTokenInfo(*curr_token);
         }
-        // main logic 
         
+        if (top_symbol->is_terminal){
+            // terminal symbol
+            
+        }
+        else{
+            // non-terminal symbol 
+
+        }
     }
     cleanup();
+
+    return parse_tree;
 }
 
 void printParseTree(TreeNode* PT, char *outfile){
